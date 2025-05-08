504afa3bd8b0738a5dc75288a14414bb12185ee5
laos0 <isonylao@gmail.com>
2025-05-08 14:42:05 -0500

    added basic content

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './feature/home/home.component';
+import { AboutUsComponent } from './feature/about-us/about-us.component';
+import { ContactUsComponent } from './feature/contact-us/contact-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutUsComponent },
+    { path: 'contact', component: ContactUsComponent },
+    { path: '**', redirectTo: '' } // Optional: redirect unknown paths to home
+];
